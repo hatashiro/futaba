1795923c903b61f8b325c99dad14496b006705de
Hyunje Jun <me@noraesae.net>
2018-12-04 02:02:33 +0900

    Add Bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Futaba',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -24,7 +24,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [],
+  css: ['bootstrap/dist/css/bootstrap.css'],
 
   /*
   ** Plugins to load before mounting the App
